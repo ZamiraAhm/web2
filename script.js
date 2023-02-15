00cf9841eacb27bf1637b98b30c85d38191bbdec
ZamiraAhm <zamiraahm1@gmail.com>
2023-02-15 01:27:25 +0100

    commit

--- script.js
+++ script.js
@@ -63,4 +63,19 @@ const form = document.getElementById('reservation-form');
               prevEl: ".swipper-button-prev",
             },
           }); */
+          var swiper = new Swiper(".slide-content",{
+            slidesPerView: 3,
+            spaceBetween: 30,
+            slidesPerGroup: 3,
+            loop: true,
+            loopFillGroupWithBlank: true,
+            pagination: {
+              el: ".swiper-pagination",
+              clickable: true,
+            },
+            navigation: {
+              nextEl: ".swipper-button-next",
+              prevEl: ".swipper-button-prev",
+            },
+          }); 
          
